18df9ab0f28e456b12c98149ea3a0c6e9f9c8e8e
julien boulicaut <julien.boulicaut@epita.fr>
2019-10-16 15:05:06 +0200

    [FIX] rendement_A construct CSV in a clean way

--- rendement_A.py
+++ rendement_A.py
@@ -7,32 +7,27 @@ import pandas as pd
 URL = os.environ["JUMP_BASE_URL"]
 AUTH = (os.environ["JUMP_USER"], os.environ["JUMP_PWD"])
 
-ids = sorted(pd.read_csv("assets.csv").dropna(subset=["value"])["id"].values.tolist())
+ids = sorted(pd.read_csv("dataset/assets.csv").dropna(subset=["value"])["id"].values.tolist())
 
 Rend = 9# Pearson correlation
 
-print("Rendement_A.csv")
-with open("Rendement_A.csv", "w") as file:
+print("rendement_A.csv")
+with open("dataset/rendement_A.csv", "w", newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["source"] + [str(_id) for _id in ids])
+    writer.writerow(["source" , "rendement"])
 
-    for current in ids:
-        payload={'_ratio':[Rend],'_asset':[current],'_becnh':"null",
-                 '_startDate':"2013-06-14",
-                 '_endDate':"2019-05-31",
-                 '_frequency':"null"}
+    reponse = requests.post(
+        URL + "/ratio/invoke",
+        auth=AUTH,
+        data="""{{
+            ratio=[{}],
+            asset={},
+            benchmark={},
+            startDate=2013-06-14,
+            endDate=2019-05-31,
+            frequency=null
+        }}""".format(Rend, ids,  ids[0]))
 
-        reponse = requests.post(
-            URL + "/ratio/invoke",
-            auth=AUTH,
-            data="""{{
-                ratio=[{}],
-                asset={},
-                benchmark={},
-                startDate=2013-06-14,
-                endDate=2019-05-31,
-                frequency=null
-            }}""".format(Rend, ids, current))
-
-        content = reponse.json()
-        writer.writerow([current] + [content[str(_id)][str(Rend)]["value"].replace(",", ".") for _id in ids])
+    content = reponse.json()
+    for _id in ids:
+        writer.writerow([_id] + [content[str(_id)][str(Rend)]["value"].replace(",", ".")])
