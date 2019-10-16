851107ef821a0924e218ecf6173cbbdf2c204316
jeremy beuvry <jeremy.beuvry@epita.fr>
2019-10-16 17:22:02 +0200

    [ADD] covariance computation and dataset generation

---
+++ covariance.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = pd.read_csv("dataset/correlation.csv")
+volatility = pd.read_csv("dataset/volatility.csv")
+id_list = data.columns[1:]
+
+covariance = data.copy()
+covariance[id_list] = covariance[id_list].apply(lambda x: x * volatility["volatility"], axis=0)
+covariance[id_list] = covariance[id_list].apply(lambda x: x * volatility["volatility"].values, axis=1)
+
+covariance.to_csv("dataset/covariance.csv")
