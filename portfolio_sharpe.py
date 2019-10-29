b2f385eaea95fe85585bbb7746592d4a01d0576c
jeremy beuvry <jeremy.beuvry@epita.fr>
2019-10-29 16:50:50 +0100

    [ADD] script to compute pushed portfolio sharp coef

---
+++ portfolio_sharpe.py
@@ -0,0 +1,27 @@
+import requests
+import csv
+import os
+import utils
+import numpy as np
+
+# Compute the sharpe coefficient of pushed portfolio
+# to be able to compare them easily if needed.
+
+def correct_float(value):
+    return value.replace(",", ".")
+
+# To prevent pushing the values by mistakes on a public repo
+base_url = os.environ["JUMP_BASE_URL"]
+auth = (os.environ["JUMP_USER"], os.environ["JUMP_PWD"])
+
+assets = utils.load_assets()
+
+for portfolio_id in [1822]:
+    url = base_url + "/portfolio/{}/dyn_amount_compo".format(portfolio_id)
+    content = requests.get(url, auth=auth).json()
+
+    portfolio = np.zeros((len(assets["info"]),))
+    for asset in content["values"]["2013-06-14"]:
+        portfolio[utils.asset_id_to_index(assets, asset["asset"]["asset"])] = asset["asset"]["quantity"]
+
+    print("{}: {}".format(content["label"], utils.sharpe(assets, portfolio)))
