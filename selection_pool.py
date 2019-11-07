35f0c62814f3cd5a400d84db31053b4a79833241
julien boulicaut <julien.boulicaut@epita.fr>
2019-11-07 12:48:58 +0100

    [ADD] new way of getting a fast selection pool

--- selection_pool.py
+++ selection_pool.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
 import csv
-from utils import load_assets, portfolio_pool_from_id_list
+from utils import load_assets, asset_id_to_index
 import utils
 
 """"
@@ -8,18 +9,28 @@ return a list of JUMP_ID of size:size_pool of the
 best assets sorted by sharpe with exactly stock_nb stocks
 """
 
-def get_pool( size_pool = 20, stock_nb= 5):
+def get_pool( val_ref = 1000000, size_pool=30, stock_nb= 20):
     full_assets = load_assets()
-    assets = full_assets["info"][["id","type","sharpe"]]
+    assets = full_assets["info"][["id","type","sharpe","value"]]
 
     assets = assets.sort_values("sharpe", 0, ascending=False)
 
     fund = assets.loc[assets["type"] == "FUND"].head(size_pool- stock_nb)
     stock = assets.loc[assets["type"] == "STOCK"].head(stock_nb)
-
+    print(stock)
+    print()
+    print(fund)
+    print()
     pool= fund["id"].values.tolist() + stock["id"].values.tolist()
 
-    return portfolio_pool_from_id_list(full_assets, pool)
+    portfolio = np.zeros(len(full_assets["info"]))
 
-assets = utils.load_assets()
-utils.push_portfolio(assets, get_pool())
+    for id in pool:
+
+        val = float(assets[assets.id == int(id)].iloc[0].iloc[0])
+        portfolio[asset_id_to_index(full_assets,id)] = int((1/size_pool)*val_ref /val)
+
+    return portfolio
+
+
+print(get_pool())
